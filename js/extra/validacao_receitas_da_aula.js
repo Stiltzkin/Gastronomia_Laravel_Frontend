7a23affafbd7e14c4c5c0c3f2e3ee76224e2d32f
Andrey Kawaguchi <andrey.kawaguchi@opet.edu.br>
2018-03-12 16:33:33 -0300

    Correções no agendamento e conclusão das aulas + botao Desagendar Aula

--- js/extra/validacao_receitas_da_aula.js
+++ js/extra/validacao_receitas_da_aula.js
@@ -1,86 +1,86 @@
 window.receitaArray = [];
 $("#btnAddReceita").click(function(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    limpaMensagens();
-    var formAdicionarReceita = $('#form_addAula');
-    var receita = obtemReceitaDoFormulario(formAdicionarReceita);
+  limpaMensagens();
+  var formAdicionarReceita = $('#form_addAula');
+  var receita = obtemReceitaDoFormulario(formAdicionarReceita);
 
-    var htmlTr = $('<tr data-id="' + receita.id + '" class="ig"></tr>');
-    var htmlTdNome = $('<td class="info-nome"><input hidden class="eachReceitaAula' + rec + '" type="text" name="receitaId" value="' + receita.id + '" /><p>' + receita.nome + '</p></td>');
-    var htmlTdQtd = $('<td class="info-quantidade"><input hidden class="eachQuantidadeReceita' + rec + '" type="text" name="quantidade_receita]" value="' + receita.quantidade + '" /><p>' + receita.quantidade + '</p></td>');
+  var htmlTr = $('<tr data-id="' + receita.id + '" class="ig"></tr>');
+  var htmlTdNome = $('<td class="info-nome"><input hidden class="eachReceitaAula" type="text" name="receitaId" value="' + receita.id + '" /><p>' + receita.nome + '</p></td>');
+  var htmlTdQtd = $('<td class="info-quantidade"><input hidden class="eachQuantidadeReceita" type="text" name="quantidade_receita]" value="' + receita.quantidade + '" /><p>' + receita.quantidade + '</p></td>');
 
-    var htmlExcluirBtn = '<td class="botao-excluir">' + receita.excluir + '</td>'
+  var htmlExcluirBtn = '<td class="botao-excluir">' + receita.excluir + '</td>'
 
-    $(htmlTdNome).appendTo(htmlTr);
-    $(htmlTdQtd).appendTo(htmlTr);
+  $(htmlTdNome).appendTo(htmlTr);
+  $(htmlTdQtd).appendTo(htmlTr);
 
-    $(htmlExcluirBtn).appendTo(htmlTr);
+  $(htmlExcluirBtn).appendTo(htmlTr);
 
-    var erros = validaReceita(receita);
-    if (erros.length > 0) {
-        exibeMensagensDeErro(erros);
-    } else {
-        $('.tabela_receita').append(htmlTr);
-    }
+  var erros = validaReceita(receita);
+  if (erros.length > 0) {
+    exibeMensagensDeErro(erros);
+  } else {
+    $('.tabela_receita').append(htmlTr);
+  }
 });
 
 // BOTAO REMOVE RECEITA DA AULA
 $(".tabela_receita").on('click', '.excluir', function() {
-    var splice = $(this).closest('tr').data('id');
+  var splice = $(this).closest('tr').data('id');
 
-    for (var i = 0; i < receitaArray.length; i++) {
-        if (receitaArray[i] == splice) {
-            receitaArray.splice(i, 1);
-            break;
-        }
+  for (var i = 0; i < receitaArray.length; i++) {
+    if (receitaArray[i] == splice) {
+      receitaArray.splice(i, 1);
+      break;
     }
-    $(this).closest('tr').remove();
+  }
+  $(this).closest('tr').remove();
 });
 
 // CAPTURA OS DADOS DO SELECT RECEITA
 function obtemReceitaDoFormulario(formAdicionarReceita) {
-    var nomeReceita = document.getElementById("receitas");
+  var nomeReceita = document.getElementById("receitas");
 
-    // BOTAO EXCLUIR GERADO
-    var htmlBotao = '<button type="button" class="excluir">Excluir</button>';
+  // BOTAO EXCLUIR GERADO
+  var htmlBotao = '<button type="button" class="excluir">Excluir</button>';
 
-    var receita = {
-        id: nomeReceita.options[nomeReceita.selectedIndex].value,
-        nome: nomeReceita.options[nomeReceita.selectedIndex].text,
-        quantidade: document.getElementById("numero_de_receitas").value,
-        excluir: htmlBotao
-    }
-    return receita;
+  var receita = {
+    id: nomeReceita.options[nomeReceita.selectedIndex].value,
+    nome: nomeReceita.options[nomeReceita.selectedIndex].text,
+    quantidade: document.getElementById("numero_de_receitas").value,
+    excluir: htmlBotao
+  }
+  return receita;
 }
 
 // VALIDAÇAO DA RECEITA
 function validaReceita(receita) {
-    var erros = [];
-    var idx = $.inArray(receita.id, receitaArray);
+  var erros = [];
+  var idx = $.inArray(receita.id, receitaArray);
 
-    if (document.getElementById("receitas").selectedIndex == 0 || document.getElementById("receitas").value == 0) {
-        erros.push("SELECIONE uma receita");
-    }
-    if (idx != -1) {
-        erros.push("A receita JA ESTÁ INSERIDO");
-    }
-    if (receita.quantidade == 0 || receita.quantidade == "") {
-        erros.push("A QUANTIDADE de receitas não pode ser em branco");
-    }
-    if (isNaN(receita.quantidade)) {
-        erros.push("A quantidade de receitas deve conter NUMERCOS APENAS");
-    }
-    if (idx == -1 && receita.quantidade != 0 && !isNaN(receita.quantidade) && document.getElementById("receitas").selectedIndex != 0) {
-        receitaArray.push(receita.id);
-        erros.length = 0;
-    }
-    return erros;
+  if (document.getElementById("receitas").selectedIndex == 0 || document.getElementById("receitas").value == 0) {
+    erros.push("SELECIONE uma receita");
+  }
+  if (idx != -1) {
+    erros.push("A receita JA ESTÁ INSERIDO");
+  }
+  if (receita.quantidade == 0 || receita.quantidade == "") {
+    erros.push("A QUANTIDADE de receitas não pode ser em branco");
+  }
+  if (isNaN(receita.quantidade)) {
+    erros.push("A quantidade de receitas deve conter NUMERCOS APENAS");
+  }
+  if (idx == -1 && receita.quantidade != 0 && !isNaN(receita.quantidade) && document.getElementById("receitas").selectedIndex != 0) {
+    receitaArray.push(receita.id);
+    erros.length = 0;
+  }
+  return erros;
 }
 
 function exibeMensagensDeErro(erros) {
-    $.each(erros, function(index, erro) {
-        var li = $("<li>" + erro + "</li>");
-        $(li).appendTo('#mensagens-erro-aula');
-    });
+  $.each(erros, function(index, erro) {
+    var li = $("<li>" + erro + "</li>");
+    $(li).appendTo('#mensagens-erro-aula');
+  });
 }
